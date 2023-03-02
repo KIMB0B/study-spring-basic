12629b9ea19d48a2d54b5db278077669f4f9a007
kimbob <axzsd96301@gmail.com>
2023-03-02 23:13:26 +0900

    Learn how to get one bean into many same type beans

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,16 +3,26 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private final @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy;
+
+    // @Autowired는 생성자가 하나면 생략 가능함
+    public OrderServiceImpl(
+            MemberRepository memberRepository,
+            /* @Qualifier("mainDiscountPolicy") 추가 시 해당 Qualifier를 지정한 FixDiscountPolicy로 지정됨
+            * 추가하지 않을 시 @Primary를 지정한 RateDiscountPolicy로 지정됨 */
+            DiscountPolicy discountPolicy
+    ) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
